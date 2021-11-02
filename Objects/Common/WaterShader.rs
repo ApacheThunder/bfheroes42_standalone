7d18671179fba36b131c6f76be12010de472d061
ApacheThunder <spenserwhaley@gmail.com>
2021-11-01 21:18:36 -0500

    Initial Commit 2
    There's gonna be a few of these. Git implodes if I try to push the
    entire thing at once. :P

---
+++ Objects/Common/WaterShader.rs
@@ -0,0 +1,10 @@
+subshader "Material0" "PatchTerrain/Water"
+{
+	lighting true;
+	lightingSpecular false;
+	materialAmbient 1 1 1;
+	materialDiffuse 1 1 1;
+	twosided true;
+	envmap true;
+	transparent true;
+}
